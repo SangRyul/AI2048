dff0b7b15b5715718a6f2d1518333fa086b73109
SangRyul <ksl970330@naver.com>
2016-11-28 01:33:28 +0900

    색깔. 오른쪽키 구현 완료

--- AI2048.c
+++ AI2048.c
@@ -7,19 +7,29 @@
 #include<time.h>
 
 
+
 #include"UI.h"
 #define RowNumber 4
 #define ColNumber 4
 
 int Board[RowNumber][ColNumber] = { 0, };
-int Randomtable[10] = { 2, 2, 2, 2, 2, 2, 4, 2, 2, 2 };
+int Randomtable[10] = { 2, 2, 2, 2, 2, 4, 2, 2, 2, 2 };
 
 
 void PrintBoard(void){
 	for (int i = 0; i < RowNumber; i++){
 		for (int j = 0; j < ColNumber; j++){
-			printf("%4d", Board[i][j]);
+			if (Board[i][j] == 2) {
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 11);
+				printf("%4d", Board[i][j]);
+			}
+			else {
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+				printf("%4d", Board[i][j]);
+				//컬러 밖으로빼기
+			}
 		}
+		printf("\n");
 		printf("\n");
 	}
 }
@@ -54,25 +64,56 @@ void DownKey(void){
 
 }
 void RightKey(void){
-	for (int i = 0; i < RowNumber ; i++){
-		for (int j = 0; j < ColNumber - 1; j++){
-			if(Board[i][j] == Board[i][j+1]){
-				Board[i][j] = 0;
-				Board[i][j + 1] = 2 * Board[i][j + 1];
 
-			}
-			else if((Board[i][j] != Board[i][j+1]) && (Board[i][j+1] !=0)){
+
+	for (int i = 0; i < RowNumber; i++) {
+
+		for (int j = 0; j <ColNumber-1; j++) {
+
+			if ((Board[i][j] != Board[i][j + 1]) && (Board[i][j + 1] != 0)) {
 				;//아무것도 하지 않는다
 			}
-			else if ((Board[i][j] != Board[i][j + 1]) && (Board[i][j + 1] == 0)){
+			else if (Board[i][j + 1] == 0) {
 				Board[i][j + 1] = Board[i][j];
+				Board[i][j] = 0;
+			}
+			else if (Board[i][j] == Board[i][j + 1]) {
+				Board[i][j] = 0;
+				Board[i][j + 1] = 2 * Board[i][j + 1];
+				if (j >= 1) {
+					Board[i][j] = Board[i][j - 1];
+					Board[i][j - 1] = 0;
+				}
+				break;
+
+			}
+
+		}
+
+	}
+
+
+
+}
+void LeftKey(void){
+
+	for (int i = 0; i < RowNumber ; i++){
+		for (int j = ColNumber -1; j >0; j--){
+			if(Board[i][j] == Board[i][j-1]){
+				Board[i][j] = 0;
+				Board[i][j - 1] = 2 * Board[i][j - 1];
+
+			}
+			else if((Board[i][j] != Board[i][j-1]) && (Board[i][j-1] !=0)){
+				;//아무것도 하지 않는다
+			}
+			else if ((Board[i][j] != Board[i][j - 1]) && (Board[i][j - 1] == 0)){
+				Board[i][j - 1] = Board[i][j];
 				Board[i][j] = 0;
 			}
 		}
 	}
 
-}
-void LeftKey(void){
 
 }
 //2048 움직이는것
@@ -104,6 +145,7 @@ int main(void){
 					system("cls");
 					RandomNumber();
 					PrintBoard();
+
 
 				}
 				if ((_kbhit() == 0) && GetAsyncKeyState(VK_DOWN) < 0) {
