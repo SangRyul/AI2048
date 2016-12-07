4922c994fa68f830417ed96b6327e6886029bba6
SangRyul <ksl970330@naver.com>
2016-12-07 21:52:19 +0900

    New logic

--- AI2048.c
+++ AI2048.c
@@ -395,3 +395,68 @@ int main(void){
 	system("pause");
 	return 0;
 }
+/*
+
+
+int removeZero(int line[4], int _flag) {
+int i, j, flag = _flag;
+
+for (i = 3; i > 0; i--) {
+if (line[i] == 0) {
+flag = 1;
+for (j = i; j > 0; j--) {
+line[j] = line[j - 1];
+}
+line[0] = 0;
+}
+}
+
+return flag;
+}
+
+int move(int line[4]) {
+int i, j, flag = 0, zero_flag = 0;
+
+for (i = 3; i > 0; i--) {
+if (line[i] == 0) {
+flag = removeZero(line, flag);
+zero_flag++;
+if (zero_flag > 4) {
+return flag;
+}
+i++;
+continue;
+}
+
+else if (line[i - 1] == 0) {
+for (j = i - 1; j >= 0; j--) {
+if (line[j] != 0) {
+line[i - 1] = line[i];
+line[i] = 0;
+flag = removeZero(line, flag);
+i++;
+break;
+}
+}
+if (j == 0) {
+return flag;
+}
+}
+else if (line[i - 1] != line[i]) {
+continue;
+}
+else if (line[i - 1] == line[i]) {
+line[i] = 2 * line[i];
+line[i - 1] = 0;
+i--;
+flag = 1;
+continue;
+}
+}
+
+
+return removeZero(line, flag);
+
+}
+
+*/
